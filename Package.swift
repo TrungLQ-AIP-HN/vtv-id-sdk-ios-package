088dcec72df64964927610271d40c1d598efa155
ducaip <ducnq@aipower.com.vn>
2026-03-25 16:00:15 +0700

    fix: rename package to VtvIdSdkBinary to avoid conflict with source repo

--- Package.swift
+++ Package.swift
@@ -1,10 +1,10 @@
 // swift-tools-version: 5.9
 import PackageDescription
 let package = Package(
-    name: "VtvIdSdk",
+    name: "VtvIdSdkBinary",
     platforms: [.iOS(.v15), .tvOS(.v16)],
     products: [
-        .library(name: "VtvIdSdk", targets: ["VtvIdSdk"])
+        .library(name: "VtvIdSdkBinary", targets: ["VtvIdSdk"])
     ],
     targets: [
         .binaryTarget(
